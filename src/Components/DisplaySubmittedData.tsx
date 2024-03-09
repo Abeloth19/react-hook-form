fe7a01286aaa3179be92ccd0a12c83c512a12f1e
Abeloth19 <prajwalmahajan258@gmail.com>
2024-03-09 22:46:26 +0530

    fixes

--- src/Components/DisplaySubmittedData.tsx
+++ src/Components/DisplaySubmittedData.tsx
@@ -18,17 +18,17 @@ const DisplaySubmittedData = forwardRef<DisplayRef, {}>((props, ref) => {
   return (
     <div className="flex flex-col space-y-2 p-5 items-start bg-[#f0eeee] h-[300px] rounded-lg text-lg font-semibold justify-start mx-auto w-[695px]">
 
-      <p className="capitalize">First Name: {data ? data.firstName : ""}</p>
-      <p className="capitalize">Last Name: {data ? data.lastName : ""}</p>
-      <p >Email Address: <span className="lowercase">{data ? data.email : ""}</span></p>
+      <p>First Name: <span className="uppercase">{data ? data.firstName : ""}</span></p>
+      <p>Last Name: <span className="uppercase">{data ? data.lastName : ""}</span></p>
+      <p>Email Address: <span className="lowercase">{data ? data.email : ""}</span></p>
       <p>Phone Number: {data ? `+91 ${data.phoneNumber}` : ""}</p>
       <p className="capitalize">Gender: {data ? data.gender : ""}</p>
       <p>Date of Birth: {data ? data.dateOfBirth : ""}</p>
-      <p className="capitalize">
+      <p>
         Tech Stack:{" "}
         {data && data.techStack
           ? data.techStack.map((tech, index) => (
-              <span key={index}>
+              <span key={index} className="uppercase">
                 {tech.name}
                 {index !== data.techStack.length - 1 ? ", " : ""}
               </span>
